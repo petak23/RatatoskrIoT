5d13cd0e774e2926ed9a9b1ff0bef5edbc18e961
petak23-laptop <petak23@gmail.com>
2023-09-13 13:22:07 +0200

    2023-09-13_04 - do API pridané ďalšie modely.

--- server/php-app/app/ApiModule/Model/Sessions.php
+++ server/php-app/app/ApiModule/Model/Sessions.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\ApiModule\Model;
 
-use Nette;
-
 /**
- * Model, ktory sa stara o tabulku value_types
+ * Model, ktory sa stara o tabulku sessions
  * 
  * Posledna zmena 13.09.2023
  * 
@@ -15,15 +13,16 @@ use Nette;
  * @copyright  Copyright (c) 2021 - 2023 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version    1.0.1
+ * @version    1.0.3
  */
-class Units extends Table
+class Sessions extends Table
 {
-	/** @var string */
-	protected $tableName = 'value_types';
 
-	public function getUnits(): array
+	/** @var string */
+	protected $tableName = 'sessions';
+
+	public function deleteSession(int $id): void
 	{
-		return $this->findAll()->order('id ASC')->fetchPairs("id", "unit");
+		$this->findBy(["device_id" => $id])->delete();
 	}
 }
